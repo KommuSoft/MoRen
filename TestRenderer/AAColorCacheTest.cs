183ffafb2c5d7aaeb3ffb52d577a6f987858d0e2
KommuSoft <vanonsem.willem@gmail.com>
2012-12-15 04:34:08 +0000

    extra test colorcache
    git-svn-id: https://subversion.assembla.com/svn/monorenderer@108 239c3ba6-4da3-4a9a-b855-b2e62a2be08a

---
+++ TestRenderer/AAColorCacheTest.cs
@@ -0,0 +1,40 @@
+//
+//  AAColorCacheTest.cs
+//
+//  Author:
+//       Willem Van Onsem <vanonsem.willem@gmail.com>
+//
+//  Copyright (c) 2012 Willem Van Onsem
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+using System;
+using NUnit.Framework;
+using Renderer;
+
+namespace TestRenderer {
+
+	[TestFixture()]
+	public class AAColorCacheTest {
+		[Test()]
+		public void MixTest () {
+			for(int i = 0; i < TestParameters.ColorTest; i++) {
+				ColorCache cc = new ColorCache(0x00);
+				uint rgb = (uint)((uint)Maths.RandomGenerator.Next()&Color.Color8Mask);
+				cc.AddColor(rgb);
+				Assert.AreEqual(rgb, cc.MixRGB(0x01));
+			}
+		}
+	}
+}
+
